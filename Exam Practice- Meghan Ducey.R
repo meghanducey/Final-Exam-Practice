913760ba20f94af6b90fe979b04fd1753154a3cd
Meghan Ducey <mkd219@lehigh.edu>
2019-12-15 23:42:04 -0500

    added and defined variables
    variables defined

---
+++ Exam Practice- Meghan Ducey.R
@@ -0,0 +1,13 @@
+############################################################
+# R script to accompany Intro to R for Business, Chapter 3 #
+# Written by Meghan Ducey                                    #
+############################################################
+
+# Still nothing to see here
+
+x<- 20
+
+# Comment!!!!
+y<- 8
+
+z<- 500000
